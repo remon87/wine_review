2a42da15168d21ae1e79aa5aa0e3b5a48c849593
remon87 <remon.eshak@evanschambers.com>
2014-09-11 19:21:03 -0400

    HW #3

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -4,9 +4,25 @@ class WinesController < ApplicationController
 		@available_at = Time.now
 		@wines = Wine.all
 	end
+	
+	def new
+		@wine = Wine.new
+	end
 
 	def show
 		@wine = Wine.find(params[:id])
 	end
+
+	def create
+		@wine = Wine.new(wine_params)
+		@wine.save
+		redirect_to @wine
+	end
+	
+	private
+
+	def wine_params
+		params.require(:wine).permit(:name, :year, :winery, :country, :varietal)
+	end		
 
 end
